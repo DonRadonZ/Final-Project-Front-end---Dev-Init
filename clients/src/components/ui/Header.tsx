90003b3c56babd64667c951d95d878dbe83f355d
RadonZ <supachai27102000@gmail.com>
2024-06-21 01:14:46 +0700

    add navigation

--- clients/src/components/ui/Header.tsx
+++ clients/src/components/ui/Header.tsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import Navigation from "./Navigation";
 
 
 export default function Header() {
     return (
         <header className="flex justify-between items-center bg-cyan-200 uppercase px-4 py-3 border-b border-stone-200 sm:px-6">
-         <Link to='/' className="tracking-widest">Clock Note</Link>   
+         <Link to='/' className="tracking-widest">Clock Note</Link>
+         <Navigation/>   
          </header>
     )
 }
