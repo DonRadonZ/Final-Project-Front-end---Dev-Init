60cb819d7eee32db4607d1ebe2c6135e0858b0a9
RadonZ <supachai27102000@gmail.com>
2024-06-21 01:15:05 +0700

    add: all navigation part

---
+++ clients/src/components/ui/Navigation.tsx
@@ -0,0 +1,18 @@
+
+
+
+export default function Navigation() {
+  return (
+    <ul>
+        <li>
+            <a href="/">Dashboard</a>
+        </li>
+        <li>
+            <a href="/todo">Todo</a>
+        </li>
+        <li>
+            <a href="/note">Note</a>
+        </li>
+    </ul>
+  )
+}
