7034460bad572fc0fac4a0b169bbc5cc1a2d112e
RadonZ <supachai27102000@gmail.com>
2024-06-21 01:14:02 +0700

    change local router

--- clients/src/main.tsx
+++ clients/src/main.tsx
@@ -2,9 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import AppLayout from './components/ui/AppLayout.tsx';
+import Todo from './page/Todo.tsx';
+import Notes from './page/Notes.tsx';
+
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <App/>,
+      },
+      {
+        path: '/todo',
+        element: <Todo/>,
+      },
+      {
+        path: '/notes',
+        element: <Notes/>,
+      },
+    ]
+  },
+]);
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
