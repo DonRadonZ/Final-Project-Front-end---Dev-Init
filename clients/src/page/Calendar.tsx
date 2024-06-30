d5e309edfee70ece7363b42d02f18a92574c3b12
RadonZ <supachai27102000@gmail.com>
2024-06-30 23:43:52 +0700

    add: calendar page

---
+++ clients/src/page/Calendar.tsx
@@ -0,0 +1,13 @@
+
+import Calendars from "../components/ui/Calendars";
+
+
+export default function CalendarPage() {
+  return (
+     
+    <div className="justify-center flex">
+      <Calendars/>
+    </div>
+    
+  )
+}
