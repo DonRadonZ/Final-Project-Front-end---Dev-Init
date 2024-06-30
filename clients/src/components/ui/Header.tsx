f11199966cd3b9ca4b913716bf614d4824e9c828
RadonZ <supachai27102000@gmail.com>
2024-06-30 23:42:30 +0700

    update header

--- clients/src/components/ui/Header.tsx
+++ clients/src/components/ui/Header.tsx
@@ -4,7 +4,7 @@ import Navigation from "./Navigation";
 
 export default function Header() {
     return (
-        <header className="flex justify-between items-center bg-cyan-200 uppercase px-4 py-3 border-b border-stone-200 sm:px-6">
+        <header className="flex justify-between items-center bg-cyan-200 uppercase px-8 py-5 border-b border-stone-200 sm:px-6">
          <Link to='/' className="tracking-widest">Clock Note</Link>
          <Navigation/>   
          </header>
