58b49aef639417114e403d37c6ba685c715565d4
RadonZ <supachai27102000@gmail.com>
2024-07-01 17:07:24 +0700

    update: screen

--- clients/src/App.tsx
+++ clients/src/App.tsx
@@ -4,10 +4,17 @@ import Calendars from "./components/ui/Calendars"
 export default function App() {
   return (
     <div>
-    <div className="flex justify-center flex-col md:flex-row md:gap-5 xl:gap-20">
+    <div className="flex justify-between flex-col md:flex-row md:gap-5 xl:gap-20 mx-6">
       
       <Calendars/>
-      <div className="bg-white">Todo</div>
+      <div className="w-[500px]">
+      <div className="flex justify-between gap-7">
+      <div className="text-center bg-white w-full  h-48 rounded-md">Progress</div>
+      <div className="text-center items-center bg-white
+      w-full h-48 rounded-md">Reward</div>
+      </div>
+      <div className="text-center bg-white w-full mt-8 h-[600px] rounded-md">Todo</div>
+      </div>
     </div>
     </div>
   )
