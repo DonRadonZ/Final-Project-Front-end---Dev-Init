f57a529c3117e45204e3834053d4b2175d1fdeef
RadonZ <supachai27102000@gmail.com>
2024-07-01 17:08:47 +0700

    add: header todo and note

--- clients/src/page/Notes.tsx
+++ clients/src/page/Notes.tsx
@@ -6,7 +6,7 @@ export default function Notes() {
       <div className='flex justify-between items-center'>
         <h1 className='font-bold text-6xl'>Note List</h1>
         
-        <a href="">Add New Note</a>
+        <a href="" className="bg-white py-3 px-5 rounded-md">New Note</a>
       </div>
     </div>
   )
--- clients/src/page/Todo.tsx
+++ clients/src/page/Todo.tsx
@@ -4,8 +4,8 @@ export default function Todo() {
   return (
     <div className="flex items-center justify-between">
       <h2 className="font-bold text-6xl">Todo List</h2>
-      <input type="text" placeholder='search...' className='bg-white' />
-      <a href="">Add Todo</a>
+      <input type="text" placeholder='search...' className='bg-white w-[800px] py-6 text-left pl-7' />
+      <a href="" className="bg-white py-3 px-5 rounded-md">Add Todo</a>
     </div>
   )
 }
