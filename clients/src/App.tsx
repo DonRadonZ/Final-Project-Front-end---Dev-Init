82c93ed9dff00a92a6edac045f05a8747a8bb8f7
RadonZ <supachai27102000@gmail.com>
2024-07-02 16:16:16 +0700

    add progress

--- clients/src/App.tsx
+++ clients/src/App.tsx
@@ -9,11 +9,14 @@ export default function App() {
       <Calendars/>
       <div className="w-[500px]">
       <div className="flex justify-between gap-7">
-      <div className="text-center bg-white w-full  h-48 rounded-md">Progress</div>
+      <div className="text-center bg-white w-full  h-48 rounded-md"><h1 className="text-xl mb-7 ">Progress</h1><div className="radial-progress  text-orange-500" style={{ "--value": "70", "--size": "7rem" }}>
+        70%</div></div>
       <div className="text-center items-center bg-white
-      w-full h-48 rounded-md">Reward</div>
+      w-full h-48 rounded-md"><h1 className="text-xl">Reward</h1></div>
       </div>
-      <div className="text-center bg-white w-full mt-8 h-[600px] rounded-md">Todo</div>
+      <div className="px-4 py-4 bg-white w-full mt-8 h-[600px] rounded-md"><h1 className="text-xl ">Today Activity</h1><hr className="mt-5"></hr>
+      
+      </div>
       </div>
     </div>
     </div>
