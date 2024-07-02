a1dbf421c697a202d2d3f97c8703cec5199decb0
RadonZ <supachai27102000@gmail.com>
2024-07-02 18:24:55 +0700

    add: modal for note

--- clients/src/page/Notes.tsx
+++ clients/src/page/Notes.tsx
@@ -1,16 +1,38 @@
+import { useState } from "react";
 import NoteList from "../components/ui/NoteList";
-
+import AddNote from "../features/note/AddNote";
+import Modal from "react-modal";
 
 
 export default function Notes() {
+  const [openAddModal, setOpenAddModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  });
+
   return (
-    <div>
+    <>
       <div className='flex justify-between items-center'>
         <h1 className='font-bold text-6xl'>Note List</h1>
         
-        <a href="" className="bg-white py-3 px-5 rounded-md">New Note</a>
+        <button className="bg-white py-3 px-5 rounded-md" onClick={() => {setOpenAddModal({isShown: true, type:"add", data: null})}}>New Note</button>
       </div>
     <NoteList/>
-    </div>
+    
+    <Modal
+    isOpen={openAddModal.isShown}
+    onRequestClose={() => {}}
+    style={{
+      overlay: {
+        backgroundColor: "rgba(0,0,0,0.2)",
+      },
+    }}
+    contentLabel=""
+    className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 "
+    >
+    <AddNote onClose={() => setOpenAddModal({isShown: false, type:"add", data: null})}/>
+    </Modal>
+    </>
   )
 }
