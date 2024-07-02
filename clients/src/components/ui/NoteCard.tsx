bb9118553ac602fb0ab4c523f5bea9765b326b16
RadonZ <supachai27102000@gmail.com>
2024-07-02 18:24:12 +0700

    update: note card part

--- clients/src/components/ui/NoteCard.tsx
+++ clients/src/components/ui/NoteCard.tsx
@@ -1,16 +1,26 @@
 
 import { HiPencilSquare, HiTrash } from 'react-icons/hi2'
 
-export default function NoteCard() {
+// interface NoteCardProp {
+
+// }
+
+export default function NoteCard({
+    title, 
+    date, 
+    content,
+    onEdit, 
+    onDelete
+}) {
   return (
     <div className='bg-green-400 rounded-lg p-4 min-h-44 flex flex-col justify-between'>
-        <h2 className='font-bold text-2xl'>Hello This is my first note</h2>
-        <p className='mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem odit placeat quis repellendus dolore praesentium, tempore nostrum consequuntur, dolorum omnis ipsam vitae eos quibusdam, possimus nobis debitis unde ea rem.</p>
+        <h2 className='font-bold text-2xl'>{title}</h2>
+        <p className='mt-2'>{content?.slice(0,60)}</p>
       <div className='flex items-center justify-between mt-2'>
-        <small>02/07/2024</small>
+        <small>{date}</small>
         <div className='flex'>
-        <HiPencilSquare className='hover:text-blue-600' />
-        <HiTrash className='hover:text-red-600'/>
+        <HiPencilSquare className='hover:text-blue-600' onClick={onEdit} />
+        <HiTrash className='hover:text-red-600' onClick={onDelete}/>
         </div>
       </div>
     </div>
