96aa126cca10623f5926a27219a1bf52ee44eb42
RadonZ <supachai27102000@gmail.com>
2024-07-02 18:24:25 +0700

    update: temporary note

--- clients/src/components/ui/NoteList.tsx
+++ clients/src/components/ui/NoteList.tsx
@@ -4,14 +4,14 @@ import NoteCard from "./NoteCard";
 export default function NoteList() {
   return (
     <div className="mt-20 grid gap-4 grid-cols-5">
-      <NoteCard/>
-      <NoteCard/>
-      <NoteCard/>
-      <NoteCard/>
-      <NoteCard/>
-      <NoteCard/>
-      <NoteCard/>
-      <NoteCard/>
+      <NoteCard title="New work" content="Lorem ipsum" date="02/07/2567" onEdit={()=> {}} onDelete={() => {}}/>
+      <NoteCard title="New work" content="Lorem ipsum" date="02/07/2567" onEdit={()=> {}} onDelete={() => {}}/>
+      <NoteCard title="New work" content="Lorem ipsum" date="02/07/2567" onEdit={()=> {}} onDelete={() => {}}/>
+      <NoteCard title="New work" content="Lorem ipsum" date="02/07/2567" onEdit={()=> {}} onDelete={() => {}}/>
+      <NoteCard title="New work" content="Lorem ipsum" date="02/07/2567" onEdit={()=> {}} onDelete={() => {}}/>
+      <NoteCard title="New work" content="Lorem ipsum" date="02/07/2567" onEdit={()=> {}} onDelete={() => {}}/>
+      <NoteCard title="New work" content="Lorem ipsum" date="02/07/2567" onEdit={()=> {}} onDelete={() => {}}/>
+      <NoteCard title="New work" content="Lorem ipsum" date="02/07/2567" onEdit={()=> {}} onDelete={() => {}}/>
     </div>
   )
 }
