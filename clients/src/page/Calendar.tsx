2478d41e38afe9b19420f27db50b4ecc48a989c7
RadonZ <supachai27102000@gmail.com>
2024-07-02 16:18:04 +0700

    add: calendar detail

--- clients/src/page/Calendar.tsx
+++ clients/src/page/Calendar.tsx
@@ -1,12 +1,28 @@
 
-import Calendars from "../components/ui/Calendars";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from '@fullcalendar/daygrid';
+
 
 
 export default function CalendarPage() {
   return (
      
-    <div className="justify-center flex">
-      <Calendars/>
+    <div>
+      <div className='flex justify-between items-center'>
+        <h1 className='font-bold text-4xl mb-4'>Calendar</h1>
+        
+        <a href="" className="bg-white py-3 px-5 rounded-md">New Event</a>
+      </div>
+      <div className="w-full mt-5 rounded-lg p-10 bg-white">
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView='dayGridMonth'
+        // weekends={false}
+        height={640}
+        // events={events}
+        // eventContent={renderEventContent}
+      />
+      </div>
     </div>
     
   )
