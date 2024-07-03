f0f5b2cee8ad01f5b5b3f6cbf4c53f586bab0f2f
RadonZ <supachai27102000@gmail.com>
2024-07-03 12:35:28 +0700

    add: edit todo part

--- clients/src/page/Calendar.tsx
+++ clients/src/page/Calendar.tsx
@@ -1,17 +1,25 @@
-
+import { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from '@fullcalendar/daygrid';
+import Modal from "react-modal";
+import AddEvent from "../features/calendar/AddEvent";
 
 
 
 export default function CalendarPage() {
+  const [openAddModal, setOpenAddModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  });
+
   return (
      
-    <div>
+    <>
       <div className='flex justify-between items-center'>
         <h1 className='font-bold text-4xl mb-4'>Calendar</h1>
         
-        <a href="" className="bg-white py-3 px-5 rounded-md">New Event</a>
+        <button className="bg-white py-3 px-5 rounded-md" onClick={() => {setOpenAddModal({isShown: true, type:"add", data: null})}}>New Event</button>
       </div>
       <div className="w-full mt-5 rounded-lg p-10 bg-white">
       <FullCalendar
@@ -23,7 +31,20 @@ export default function CalendarPage() {
         // eventContent={renderEventContent}
       />
       </div>
-    </div>
+      <Modal
+    isOpen={openAddModal.isShown}
+    onRequestClose={() => {}}
+    style={{
+      overlay: {
+        backgroundColor: "rgba(0,0,0,0.2)",
+      },
+    }}
+    contentLabel=""
+    className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 "
+    >
+      <AddEvent onClose={() => setOpenAddModal({isShown: false, type:"add", data: null})}/>
+    </Modal>
+    </>
     
   )
 }
--- clients/src/page/Todo.tsx
+++ clients/src/page/Todo.tsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
+import AddEditTodo from "../features/todo/AddEditTodo";
+import Modal from "react-modal";
 
 
 export default function Todo() {
+  const [openAddModal, setOpenAddModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  });
+
   return (
-    <div className="flex items-center justify-between">
+    <>
+      <div className="flex items-center justify-between">
       <h2 className="font-bold text-6xl">Todo List</h2>
       <input type="text" placeholder='search...' className='bg-white w-[800px] py-6 text-left pl-7' />
-      <a href="" className="bg-white py-3 px-5 rounded-md">Add Todo</a>
-    </div>
+      <button className="bg-white py-3 px-5 rounded-md" onClick={() => {setOpenAddModal({isShown: true, type:"add", data: null})}}>Add Todo</button>
+      </div>
+      <Modal
+      isOpen={openAddModal.isShown}
+      onRequestClose={() => {}}
+      style={{
+        overlay: {
+          backgroundColor: "rgba(0,0,0,0.2)",
+        },
+      }}
+      contentLabel=""
+      className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 "
+      >
+      <AddEditTodo onClose={() => setOpenAddModal({isShown: false, type:"add", data: null})}/>
+      </Modal>
+    </>
   )
 }
