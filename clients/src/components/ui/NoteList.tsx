35d6f4cf99b2808339348d597861023ffb44aea3
RadonZ <supachai27102000@gmail.com>
2024-07-04 18:27:47 +0700

    update note list

--- clients/src/components/ui/NoteList.tsx
+++ clients/src/components/ui/NoteList.tsx
@@ -1,17 +1,13 @@
 import NoteCard from "./NoteCard";
 
 
-export default function NoteList() {
+export default function NoteList({notes}) {
   return (
     <div className="mt-20 grid gap-4 grid-cols-5">
-      <NoteCard title="New work" content="Lorem ipsum" date="02/07/2567" onEdit={()=> {}} onDelete={() => {}}/>
-      <NoteCard title="New work" content="Lorem ipsum" date="02/07/2567" onEdit={()=> {}} onDelete={() => {}}/>
-      <NoteCard title="New work" content="Lorem ipsum" date="02/07/2567" onEdit={()=> {}} onDelete={() => {}}/>
-      <NoteCard title="New work" content="Lorem ipsum" date="02/07/2567" onEdit={()=> {}} onDelete={() => {}}/>
-      <NoteCard title="New work" content="Lorem ipsum" date="02/07/2567" onEdit={()=> {}} onDelete={() => {}}/>
-      <NoteCard title="New work" content="Lorem ipsum" date="02/07/2567" onEdit={()=> {}} onDelete={() => {}}/>
-      <NoteCard title="New work" content="Lorem ipsum" date="02/07/2567" onEdit={()=> {}} onDelete={() => {}}/>
-      <NoteCard title="New work" content="Lorem ipsum" date="02/07/2567" onEdit={()=> {}} onDelete={() => {}}/>
+      {notes.map((note, index) =>( 
+        <NoteCard key={index} title={note.title} content={note.description} date={note.date} onEdit={()=> {}} onDelete={() => {}}/>
+      ))}
+      {/* <NoteCard title="New work" content="Lorem ipsum" date="02/07/2567" onEdit={()=> {}} onDelete={() => {}}/> */}
     </div>
   )
 }
