8e86ad9a38be461f4a498d579c9cbb05649e0a02
Supachai Sinkaseam <supachai27102000@gmail.com>
2024-07-06 01:27:10 +0700

    update: edit part

--- clients/src/components/ui/TodoList.tsx
+++ clients/src/components/ui/TodoList.tsx
@@ -1,11 +1,18 @@
 import TodoCard from "./TodoCard";
 
 
-export default function TodoList({onEdit, onDelete ,todos}: any) {
+export default function TodoList({onEdit, onOpen, onDelete ,todos}: any) {
   return (
     <div className="mt-20 grid gap-4 grid-cols-5">
     {todos.map((todo, index) => (
-      <TodoCard key={index} title={todo.title} detail={todo.detail} date={todo.date} onEdit={() => onEdit(index)} onDelete={() => onDelete(index)}/>
+      <TodoCard
+        key={index}
+        title={todo.title}
+        detail={todo.detail}
+        date={todo.date}
+        onEdit={() => onEdit(index)}
+        onDelete={() => onDelete(index)}
+      />
     ))}
     </div>
   )
