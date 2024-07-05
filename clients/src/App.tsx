6059f6775482d36afa1dfc7f8afcfdbe25169336
RadonZ <supachai27102000@gmail.com>
2024-07-05 18:29:12 +0700

    add: todo and Event

--- clients/src/App.tsx
+++ clients/src/App.tsx
@@ -1,12 +1,29 @@
-import Calendars from "./components/ui/Calendars"
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from '@fullcalendar/daygrid'
+import useCalendarLocalStorage from "./features/calendar/useCalendarLocalStorage";
+import useTodoLocalStorage from "./features/todo/useTodoLocalStorage";
+import UncheckedTodo from "./features/todo/UncheckedTodo";
 
 
 export default function App() {
+  const [todos] = useTodoLocalStorage([],'todos');
+  // const uncompletedTasks = todos.filter(todo => todo.isChecked).length;
+  const [events] = useCalendarLocalStorage([], 'calendar');
+
   return (
     <div>
     <div className="flex justify-between flex-col md:flex-row md:gap-5 xl:gap-20 mx-6">
       
-      <Calendars/>
+    <div className="bg-white w-[84rem] shadow rounded-lg p-10">
+    <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView='dayGridMonth'
+        // weekends={false}
+        height={720}
+        events={events}
+        // eventContent={renderEventContent}
+      />
+    </div>
       <div className="w-[500px]">
       <div className="flex justify-between gap-7">
       <div className="text-center bg-white w-full  h-48 rounded-md"><h1 className="text-xl mb-7 ">Progress</h1>
@@ -16,7 +33,9 @@ export default function App() {
       w-full h-48 rounded-md"><h1 className="text-xl">Reward</h1></div>
       </div>
       <div className="px-4 py-4 bg-white w-full mt-8 h-[600px] rounded-md"><h1 className="text-xl ">Today Activity</h1><hr className="mt-5"></hr>
-      
+      {todos.map((todo, index) => (
+        <UncheckedTodo key={index} todo={todo}/>
+        ))}
       </div>
       </div>
     </div>
