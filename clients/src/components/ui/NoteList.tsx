33e061701c64a68d7bf8c0f74810290cbfcb2afc
RadonZ <supachai27102000@gmail.com>
2024-07-05 18:29:44 +0700

    add: on delete index

--- clients/src/components/ui/NoteList.tsx
+++ clients/src/components/ui/NoteList.tsx
@@ -1,17 +1,12 @@
 import NoteCard from "./NoteCard";
 
 
-export default function NoteList({ notes }) {
+export default function NoteList({ notes, onDelete }) {
   
-  function onDeleteNote(index) {
-    
-  }
-
-
   return (
     <div className="mt-20 grid gap-4 grid-cols-5">
       {notes.map((note, index) =>( 
-        <NoteCard key={index} title={note.title} content={note.description} date={note.date} onEdit={()=> {}} onDelete={() => {}}/>
+        <NoteCard key={index} title={note.title} content={note.description} date={note.date} onEdit={()=> {}} onDelete={() => onDelete(index)}/>
       ))}
     </div>
   )
