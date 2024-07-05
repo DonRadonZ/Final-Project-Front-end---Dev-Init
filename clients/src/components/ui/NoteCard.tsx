bd76ad1b4ec4d2263dfc2e3cea71bdfdcbac4d78
RadonZ <supachai27102000@gmail.com>
2024-07-05 18:29:26 +0700

    add: index and moment

--- clients/src/components/ui/NoteCard.tsx
+++ clients/src/components/ui/NoteCard.tsx
@@ -1,4 +1,5 @@
 
+import moment from 'moment'
 import { HiPencilSquare, HiTrash } from 'react-icons/hi2'
 
 // interface NoteCardProp {
@@ -10,14 +11,15 @@ export default function NoteCard({
     date, 
     content,
     onEdit, 
-    onDelete
+    onDelete,
+    index
 }: any) {
   return (
-    <div className='bg-green-400 rounded-lg p-4 min-h-44 flex flex-col justify-between'>
+    <div className='bg-green-400 rounded-lg p-4 min-h-44 flex flex-col justify-between' key={index}>
         <h2 className='font-bold text-2xl'>{title}</h2>
         <p className='mt-2'>{content?.slice(0,60)}</p>
       <div className='flex items-center justify-between mt-2'>
-        <small>{date}</small>
+        <small>{moment(date).format("DD MMM YYYY")}</small>
         <div className='flex'>
         <HiPencilSquare className='hover:text-blue-600' onClick={onEdit} />
         <HiTrash className='hover:text-red-600' onClick={onDelete}/>
