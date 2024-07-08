44bd47ceb57a0e360547f3dda306e5a282c0c59a
RadonZ <supachai27102000@gmail.com>
2024-07-08 18:13:18 +0700

    add: darkmode

---
+++ clients/src/context/DarkmodeContext.tsx
@@ -0,0 +1,45 @@
+import { createContext, ReactNode, useContext, useEffect } from "react";
+import { useLocalStorageState } from "./useLocalStorageState";
+
+
+type DarkModeCtxType = {
+    isDarkMode: boolean;
+    toggleDarkMode: () => void
+  }
+
+const DarkModeContext = createContext<DarkModeCtxType>({isDarkMode: false,
+ toggleDarkMode: () => {}
+}
+)
+
+function DarkModeProvider({children}:{children: ReactNode}){
+    const [isDarkMode, setIsDarkMode] = useLocalStorageState(false,"isDarkMode")
+
+    useEffect(function(){
+        if(isDarkMode){
+            document.documentElement.classList.add("dark");
+        }
+        if(!isDarkMode){
+            document.documentElement.classList.remove("dark");
+        }
+    },[isDarkMode]);
+
+    function toggleDarkMode(){
+        setIsDarkMode((isDark: boolean) => !isDark)
+    }
+
+    return(
+        <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+            {children}
+        </DarkModeContext.Provider>
+    )
+}
+
+function useDarkMode(){
+    const context = useContext(DarkModeContext)
+    if (context === undefined) throw new Error('DarkModeContext was outside of DarkModeProvider')
+    return context;
+}
+
+export {DarkModeProvider, useDarkMode}
+
