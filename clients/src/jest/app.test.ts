339416c7326e91f320eca8615aa87e4d5da8975a
RadonZ <supachai27102000@gmail.com>
2024-07-08 18:23:04 +0700

    add: test

---
+++ clients/src/jest/app.test.ts
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react"
+import { BrowserRouter } from "react-router-dom";
+
+const mockNotes = [
+    {date:"2024-07-08", title:"Test1", detail:"Test of note 1"},
+];
+
+
+test('renders calendar, note and todo list',() =>{
+    render(
+        <BrowserRouter>
+    )
+})
