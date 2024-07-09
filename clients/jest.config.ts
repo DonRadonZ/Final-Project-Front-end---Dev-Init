881639251eeab01797118088dda7a07ffcad5b93
RadonZ <supachai27102000@gmail.com>
2024-07-09 13:07:07 +0700

    update: setup part

--- clients/jest.config.ts
+++ clients/jest.config.ts
@@ -1,34 +1,18 @@
-import type {Config} from '@jest/types';
-// Sync object
+import type { Config } from '@jest/types';
+
 const config: Config.InitialOptions = {
-  verbose: true,
-  transform: {
-  '^.+\\.tsx?$': 'ts-jest',
-  },
-};
-export default config;
-
-export {};
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
   preset: 'ts-jest',
-  moduleNameMapper: {
-
-      // if your using tsconfig.paths thers is no harm in telling jest
-    '@components/(.*)$': '<rootDir>/src/components/$1',
-    '@/(.*)$': '<rootDir>/src/$1',
-      
-      // mocking assests and styling
-    '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/tests/mocks/fileMock.ts',
-    '^.+\\.(css|less|scss|sass)$': '<rootDir>/tests/mocks/styleMock.ts',
-    /* mock models and services folder */
-    '(assets|models|services)': '<rootDir>/tests/mocks/fileMock.ts',
-  },
-   // to obtain access to the matchers.
-  setupFilesAfterEnv: ['./tests/setupTests.ts'],
-      
+  testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  modulePaths: ['<rootDir>'],
-  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: ['**/__tests__/**/*.test.(ts|js)'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // Optional setup file
 };
+
+export default config;
